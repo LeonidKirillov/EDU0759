be28bb8b5ea8a9863e1dab3753b9474883ae40d5
Leonid <kirillov.leo@gmail.com>
2021-06-17 12:09:06 +0300

    решена задача 2.1

---
+++ Task_2/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        /*int [] nums = {5,3,9,4,87,6};
+        int sum = 0;
+        for (int i = 0; i < 6; i++){
+            sum+=nums[i];
+        }
+        System.out.println(sum/6);*/
+
+            String s1 = "Мама";
+            String s2 = "Мыла";
+            String s3 = "Раму";
+            System.out.println(s1 + s2 + s3);
+            System.out.println(s1 + s3 + s2);
+            System.out.println(s2 + s1 + s3);
+            System.out.println(s2 + s3 + s1);
+            System.out.println(s3 + s1 + s2);
+            System.out.println(s3 + s2 + s1);
+
+
+    }
+}
